fe8eb6e4d6a852023f6d65aa0f361bdd15675999
Rocio Carle <rociocarle@yahoo.com.ar>
2023-02-08 10:30:09 -0300

    probando

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const port = process.env.PORT ||3000;
 
-
-app.listen (process.env.PORT ||3000, () =>
-{console.log('Servidor funcionando bien');});
+app.listen (port, () => console.log(`Servidor funcionando bien en puerto ${port}'`));
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
